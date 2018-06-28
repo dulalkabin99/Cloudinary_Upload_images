2855c2580910f889b22d0a76e52cf8b3b0198d1c
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-28 17:09:13 -0400

    good commit

---
+++ src/main/java/com/dulal/spring_12/ActorRepo.java
@@ -0,0 +1,6 @@
+package com.dulal.spring_12;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepo extends CrudRepository<Actor, Long> {
+}
---
+++ src/main/java/com/dulal/spring_12/HomeController.java
@@ -0,0 +1,54 @@
+package com.dulal.spring_12;
+
+
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepo actorRepo;
+
+    @Autowired
+    CloudnaryConfig cloudc;
+
+    @RequestMapping("/")
+    public String listActors(Model model){
+        model.addAttribute("actors", actorRepo.findAll());
+        return "list";
+
+    }
+
+    @GetMapping("/add")
+    public String newActor( Model model){
+        model.addAttribute("actor", new Actor());
+        return "form";
+
+    }
+
+    @PostMapping("/add")
+    public String processActor(@ModelAttribute Actor actor, @RequestParam("file")MultipartFile file){
+        if (file.isEmpty()){
+            return "redirect:/add";
+        }
+        try{
+            Map uploadResult = cloudc.upload(file.getBytes(), ObjectUtils.asMap("resourcetype", "auto"));
+            actor.setHeadshot(uploadResult.get("url").toString());
+            actorRepo.save(actor);
+        }catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+        return  "redirect:/";
+
+
+    }
+
+}
